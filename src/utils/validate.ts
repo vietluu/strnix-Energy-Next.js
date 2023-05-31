34cbc8c8ca5789af090a643442adfddf98e37498
vietluu <74524082+vietluu@users.noreply.github.com>
2023-05-31 18:04:58 +0700

    feature/contact done

---
+++ src/utils/validate.ts
@@ -0,0 +1,5 @@
+export const emailValiate = (value: string) => {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    return re.test(value)
+}
