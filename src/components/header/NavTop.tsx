6b4418d6aff960f0ebd4ea43a3ea6ccb54a87d8b
vietluu <74524082+vietluu@users.noreply.github.com>
2023-07-05 13:42:07 +0700

    fix routes

--- src/components/header/NavTop.tsx
+++ src/components/header/NavTop.tsx
@@ -1,6 +1,9 @@
 import React, { JSX } from 'react';
 import Link from 'next/link';
 import Logo from '../Logo';
+import { routes } from './routes';
+import { routeType } from '@/types/routesType';
+
 export default function NavTop(): JSX.Element {
   return (
     <nav className="header-top clearfix">
@@ -9,97 +12,31 @@ export default function NavTop(): JSX.Element {
           <Logo />
         </div>
         <div className="main-menu clearfix">
-          <ul className="menu ">
-            <li>
-              <Link href="/" className="hover1 ">
-                <strong>HOME</strong>
-              </Link>
-              <ul className="sub-menu">
-                <li>
-                  <Link href="# ">Home Solar Energy</Link>
-                </li>
-                <li>
-                  <Link href="# ">Home Wind Power</Link>
-                </li>
-                <li>
-                  <Link href="# ">Home Bio Power</Link>
-                </li>
-                <li>
-                  <Link href="# ">Home Fuel Power</Link>
-                </li>
-                <li>
-                  <Link href="# ">Home Hydro Power</Link>
-                </li>
-              </ul>
+        <ul className="menu">
+        {
+          routes.map((value: routeType) => (
+            <li key={value.label}>
+              <Link  href={`${value.link}`} className="hover1 ">
+                <strong>{value.label}</strong>
+                </Link>
+                {
+                  value.subDomain.length > 0 && (
+                    <ul className="sub-menu ">
+                      {
+                        value.subDomain.map((sub) => (
+                          <li key={sub.label}>
+                            <Link href={`${sub.label}`}>{ sub.label}</Link>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  )
+                }
+          
             </li>
-            <li>
-              <Link href="about" className="hover1 ">
-                <strong>ABOUT US</strong>
-              </Link>
-              <ul className="sub-menu ">
-                <li>
-                  <Link href="about">ABOUT US</Link>
-                </li>
-                <li>
-                  <Link href="# ">{"FAQ's"}</Link>
-                </li>
-                <li>
-                  <Link href="# ">Delicated Project</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="services" className="hover1">
-                <strong>SERVICES</strong>
-              </Link>
-              <ul className="sub-menu ">
-                <li>
-                  <Link href="# ">Our Services</Link>
-                </li>
-                <li>
-                  <Link href="# ">Commercial Services</Link>
-                </li>
-                <li>
-                  <Link href="# ">Panel Installation</Link>
-                </li>
-                <li>
-                  <Link href="# ">Replacement Upgrade</Link>
-                </li>
-                <li>
-                  <Link href="# ">Residential EV Charges</Link>
-                </li>
-                <li>
-                  <Link href="# ">Solar Maintenance</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="projects" className="hover1 ">
-                <strong>PROJECTS</strong>
-              </Link>
-            </li>
-            <li>
-              <Link href="news" className="hover1 ">
-                <strong>NEWS</strong>
-              </Link>
-              <ul className="sub-menu ">
-                <li>
-                  <Link href="# ">Blog Grid Style</Link>
-                </li>
-                <li>
-                  <Link href="# ">Blog with Sidebar</Link>
-                </li>
-                <li>
-                  <Link href="# ">Blog Post Details</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="contact" className="hover1 ">
-                <strong>CONTACT</strong>
-              </Link>
-            </li>
-          </ul>
+          ))
+        }
+      </ul>
         </div>
       </div>
     </nav>
